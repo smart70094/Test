42ca14c5aa84fa1812d77582173f1f50032effbb
Jim <b10323024@yuntech.edu.tw>
2017-08-27 12:28:17 +0800

    algorithm update

--- app/src/main/java/com/example/hp/test/MainActivity.java
+++ app/src/main/java/com/example/hp/test/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("123");
+        algorithm a=new algorithm();
     }
 }
